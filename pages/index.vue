8365b37dc9f6d44ee52b5958e6e2fa81aec97e96
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-03-22 05:54:03 +0500

    some change

--- pages/index.vue
+++ pages/index.vue
@@ -10,36 +10,11 @@
         <post />
         <ReviewsSec />
         <ContactUs />
-        <pageFooter />
+        <PageFooter />
     </div>
 </template>
 
-<script>
-import PageNavbar from '@/components/PageNavbar.vue';
-import HeroSection from '@/components/HeroSection.vue';
-import AboutUs from '@/components/AboutUs.vue';
-import SignMeSec from '@/components/SignMeSec.vue';
-import Trips from '@/components/Trips.vue';
-import ExploreTrip from '@/components/ExploreTrip.vue';
-import FindAdventure from '@/components/FindAdventure.vue';
-import post from '@/components/post.vue';
-import ReviewsSec from '@/components/ReviewsSec.vue';
-import ContactUs from '@/components/ContactUs.vue';
-import pageFooter from '@/components/pageFooter.vue';
+<script setup lang="ts">
 
-export default {
-    components: {
-        PageNavbar,
-        HeroSection,
-        AboutUs,
-        SignMeSec,
-        Trips,
-        SignMeSec,
-        SignMeSec,
-        post,
-        ReviewsSec,
-        ContactUs,
-        pageFooter
-    }
-};
+
 </script>
