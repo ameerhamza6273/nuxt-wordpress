63796bb4adb96eb817bd39e40de15349b37384ab
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-03-25 10:40:30 +0500

    some change

--- components/FindAdventure.vue
+++ components/FindAdventure.vue
@@ -73,7 +73,7 @@
           <option>Hard</option>
         </select>
       </div>
-      <div class="md:hidden flex justify-center bg-[#404857e6] p-3">
+      <div class="md:hidden flex justify-center bg-[#404857e6] p-3 px-5">
         <button @click="showModal = true"
           class="bg-[#afb1b4] rounded-xl border border-[#414141]  w-full py-3 text-lg font-sebibold rounded-md shadow-lg">
           <i class="fas fa-search mr-3"></i> Start Your Search
