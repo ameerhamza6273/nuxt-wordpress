621c6a875a0dfbfcd7c117e8bd5ab80caff37068
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-03-29 11:34:19 +0500

    slide images

--- components/HeroSecSlider.vue
+++ components/HeroSecSlider.vue
@@ -2,8 +2,8 @@
     <div class="bg-transparent mt-10 md:mt-0">
         <!-- Swiper Slider -->
         <swiper :navigation="{ nextEl: '.nextArrow', prevEl: '.prevArrow' }" :pagination="{ clickable: true }"
-            :spaceBetween="20" :slidesPerView="2.5" :modules="[Navigation]" :loop="true"
-            class="w-full relative" style="padding-bottom: 70px !important;" :breakpoints="{
+            :spaceBetween="20" :slidesPerView="2.5" :modules="[Navigation]" :loop="true" class="w-full relative"
+            style="padding-bottom: 70px !important;" :breakpoints="{
                 340: { slidesPerView: 1.5 },
                 600: { slidesPerView: 2 },
                 1124: { slidesPerView: 2.5 }
@@ -23,23 +23,25 @@
             <!-- Swiper Slides -->
             <swiper-slide v-for="(data, index) in pagesLinks" :key="index">
                 <div class="rounded-xl mx-3 min-h-[300px] overflow-hidden relative"
-    :style="{ backgroundImage: `url(${data.bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
+                    :style="{ backgroundImage: `url(${data.bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
 
-    <!-- Gradient Overlay -->
-    <div class="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent pointer-events-none"></div>
+                    <!-- Gradient Overlay -->
+                    <div
+                        class="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent pointer-events-none">
+                    </div>
 
-    <div class="p-6 px-7 z-10 flex flex-col justify-end min-h-[300px] relative">
-        <!-- Dynamic Data -->
-        <p class="text-lg text-white text-center z-20">
-            <b>{{ data.title }}</b><br />
-            {{ data.description }}
-        </p>
-        <NuxtLink :to="data.link"
-            class="bg-[#ffffff1f] mt-3 delay-300 text-white rounded-xl shadow-md py-2 px-4 block font-medium text-center border-2 border-white cursor-pointer z-20">
-            {{ data.btnText }}
-        </NuxtLink>
-    </div>
-</div>
+                    <div class="p-6 px-7 z-10 flex flex-col justify-end min-h-[300px] relative">
+                        <!-- Dynamic Data -->
+                        <p class="text-lg text-white text-center z-20">
+                            <b>{{ data.title }}</b><br />
+                            {{ data.description }}
+                        </p>
+                        <NuxtLink :to="data.link"
+                            class="bg-[#ffffff1f] mt-3 delay-300 text-white rounded-xl shadow-md py-2 px-4 block font-medium text-center border-2 border-white cursor-pointer z-20">
+                            {{ data.btnText }}
+                        </NuxtLink>
+                    </div>
+                </div>
 
             </swiper-slide>
         </swiper>
@@ -56,11 +58,11 @@ import { ref } from "vue";
 
 const pagesLinks = ref([
     { bgImage: "/slider-bg-1.png", title: "Druk Path Trek:", description: "The Ultimate Bhutan Trekking Adventure", btnText: "Book Now", link: "/druk-path" },
-    { bgImage: "/slider-bg-1.png", title: "Dolomites of Cortina:", description: "Guided Hiking Adventure", btnText: "Book Now", link: "/dolomites" },
-    { bgImage: "/slider-bg-1.png", title: "Tour du Mont Blanc:", description: "A Complete 9-Day Alpine Adventure", btnText: "Book Now", link: "/mont-blanc" },
+    { bgImage: "/slider-bg-2.png", title: "Dolomites of Cortina:", description: "Guided Hiking Adventure", btnText: "Book Now", link: "/dolomites" },
+    { bgImage: "/slider-bg-3.png", title: "Tour du Mont Blanc:", description: "A Complete 9-Day Alpine Adventure", btnText: "Book Now", link: "/mont-blanc" },
     { bgImage: "/slider-bg-1.png", title: "Druk Path Trek:", description: "The Ultimate Bhutan Trekking Adventure", btnText: "Book Now", link: "/druk-path" },
-    { bgImage: "/slider-bg-1.png", title: "Dolomites of Cortina:", description: "Guided Hiking Adventure", btnText: "Book Now", link: "/dolomites" },
-    { bgImage: "/slider-bg-1.png", title: "Tour du Mont Blanc:", description: "A Complete 9-Day Alpine Adventure", btnText: "Book Now", link: "/mont-blanc" },
+    { bgImage: "/slider-bg-2.png", title: "Dolomites of Cortina:", description: "Guided Hiking Adventure", btnText: "Book Now", link: "/dolomites" },
+    { bgImage: "/slider-bg-3.png", title: "Tour du Mont Blanc:", description: "A Complete 9-Day Alpine Adventure", btnText: "Book Now", link: "/mont-blanc" },
 ]);
 </script>
 
