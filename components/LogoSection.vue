e77d08f49b13bc70a61e361fe0c3b18dd0021714
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-04-06 12:37:54 +0500

    add links

--- components/LogoSection.vue
+++ components/LogoSection.vue
@@ -22,22 +22,36 @@ const movie = computed(() => props.movies.find((m) => m.id === movieId));
 
       <div class="max-w-[990px] md:mx-auto py-12 ">
         <div class="flex flex-col sm:flex-row flex-wrap items-center justify-around gap-10">
-          <NuxtImg :src="movie.acf.logo_one" alt=""
-            class="w-100 max-w-[150px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
-          <NuxtImg :src="movie.acf.logo_two" alt=""
-            class="w-100 max-w-[210px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
-          <NuxtImg :src="movie.acf.logo_three" alt=""
-            class="w-100 max-w-[180px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
+
+          <NuxtLink to="https://airalo.pxf.io/c/6008666/2626073/15608?p.code=XTESIM15">
+            <NuxtImg :src="movie.acf.logo_one" alt=""
+              class="w-100 max-w-[150px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
+          </NuxtLink>
+
+          <NuxtLink to="https://partner.globalrescue.com/x-trekkers/index.html">
+            <NuxtImg :src="movie.acf.logo_two" alt=""
+              class="w-100 max-w-[210px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
+          </NuxtLink>
+          <NuxtLink to="https://www.worldtrips.com/buynow?referid=25649">
+            <NuxtImg :src="movie.acf.logo_three" alt=""
+              class="w-100 max-w-[180px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
+          </NuxtLink>
           <NuxtImg :src="movie.acf.logo_four" alt=""
             class="w-100 max-w-[160px] my-3 basis-1/2 md:basis-1/3 lg:basis-1/4" />
-    
+
         </div>
         <div class="flex flex-col sm:flex-row flex-wrap items-center justify-around gap-10 mt-4">
-         
+
           <NuxtImg :src="movie.acf.logo_five" alt="" class="w-100 max-w-[140px] my-3" />
           <NuxtImg :src="movie.acf.logo_six" alt="" class="w-100 max-w-[105px] my-3" />
-          <NuxtImg :src="movie.acf.logo_seven" alt="" class="w-100 max-w-[120px] my-3" />
-          <NuxtImg :src="movie.acf.logo_eight" alt="" class="w-100 max-w-[210px] my-3" />
+
+          <NuxtLink to="https://www.trip.com/t/sX1zbvatBW1">
+            <NuxtImg :src="movie.acf.logo_seven" alt="" class="w-100 max-w-[120px] my-3" />
+          </NuxtLink>
+
+          <NuxtLink to="https://www.booking.com/hotel/index.html?aid=2396338">
+            <NuxtImg :src="movie.acf.logo_eight" alt="" class="w-100 max-w-[210px] my-3" />
+          </NuxtLink>
         </div>
 
 
