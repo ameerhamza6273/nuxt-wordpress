5b174d6b9aeb8cfadb9243e479a6157f3c6aa4f3
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-04-09 03:42:03 +0500

    change

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,6 @@ const movies = ref(null);
 const dedicatedTeam = ref(null);
 const headerFooter = ref(null);
 const posts = ref(null);
-const heroSlider = ref(null); 
 const tripsPages = ref(null);
 
 
@@ -17,21 +16,18 @@ async function fetchData() {
   const [dedicatedRes, dedicatedErr] = await getDedicatedTeam();
   const [headerFooterRes, headerFooterErr] = await getHeaderFooter();
   const [postsRes, postsErr] = await getPosts();
-  const [heroSliderRes, heroSliderErr] = await getHeroSlider(); // Fetch HeroSlider data
   const [tripsPagesRes, tripsPagesErr] = await getTripsPages();
 
   if (moviesRes) movies.value = moviesRes;
   if (dedicatedRes) dedicatedTeam.value = dedicatedRes;
   if (headerFooterRes) headerFooter.value = headerFooterRes;
   if (postsRes) posts.value = postsRes;
-  if (heroSliderRes) heroSlider.value = heroSliderRes; // Assign HeroSlider data
   if (tripsPagesRes) tripsPages.value = tripsPagesRes;
 
   if (moviesErr) console.error("Error fetching movies:", moviesErr);
   if (dedicatedErr) console.error("Error fetching dedicated team:", dedicatedErr);
   if (headerFooterErr) console.error("Error fetching header & footer:", headerFooterErr);
   if (postsErr) console.error("Error fetching posts:", postsErr);
-  if (heroSliderErr) console.error("Error fetching HeroSlider:", heroSliderErr); // Log HeroSlider errors
   if (tripsPagesErr) console.error("Error fetching trips pages:", tripsPagesErr);
 
   
@@ -48,7 +44,7 @@ onMounted(fetchData);
     </article>
     <article v-else>
       <PageNavbar :headerData="headerFooter" />
-      <HeroSection :movies="movies" :heroSlider="heroSlider" :tripData="tripsPages" />
+      <HeroSection :movies="movies"  :tripData="tripsPages" />
       
       <AboutUs :movies="movies" />
       <LogoSection :movies="movies" />
