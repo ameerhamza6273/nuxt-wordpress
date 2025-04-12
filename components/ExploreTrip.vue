1485258083da28fdcdc56bac67bbb64500a38565
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-04-12 09:09:45 +0500

    change

--- components/ExploreTrip.vue
+++ components/ExploreTrip.vue
@@ -22,7 +22,7 @@ const highlightedTrips = computed(() =>
 
 </script>
 <template>
-  <div class=" bg-[#0D0D0D]" id="exploreTrips" >
+  <div class=" bg-[#0D0D0D]" id="exploreTrips">
     <div class="grid grid-cols-1  md:grid-cols-11 mx-auto">
       <div v-if="movie" class="col-span-1 md:col-span-5 self-center px-6 md:px-16 py-16 text-center md:text-left ">
         <h3 class="text-white  text-xl lg:text-2xl mb-6 lg:mb-16 uppercase border-b inline-block ">
@@ -35,20 +35,35 @@ const highlightedTrips = computed(() =>
         <p class="text-lg text-white  mt-5">{{ movie.acf.description }} </p>
 
       </div>
-      
-      <div v-if="tripData" class="col-span-2 hidden md:block" v-for="trip in highlightedTrips" :key="trip.id">
-        <NuxtLink :to="`/trip/${trip.id}`">
-          <NuxtImg :src="trip.acf.explore_trip_desktop" alt="Druk Path Logo" class="w-full hover:opacity-[0.7] cursor-pointer" />
-        </NuxtLink>
-      </div>
+
+      <div v-if="tripData" class="col-span-2 hidden md:block relative" v-for="trip in highlightedTrips"
+          :key="trip.id">
+  <NuxtLink :to="`/trip/${trip.id}`">  
+    <NuxtImg :src="trip.acf.explore_trip_desktop" alt="Druk Path Logo"
+      class="w-full hover:opacity-[0.7] cursor-pointer" />
+    <div class="absolute right-12 bottom-0 transform translate-y-0 rotate-90 origin-right">
+      <h2 class="text-white uppercase text-5xl font-bold  tracking-wider whitespace-nowrap">
+        {{ trip.acf.trip_title }}
+      </h2>
+    </div>
+  </NuxtLink>
+</div>
 
 
     </div>
     <div v-if="tripData" class="block md:hidden">
-      <div class="flex" >
+      <div class="flex">
         <NuxtLink :to="`/trip/${trip.id}`" class="w-1/3" v-for="trip in highlightedTrips" :key="trip.id">
-          <NuxtImg :src="trip.acf.explore_trip_mobile" alt="Druk Path Logo"
-            class="w-full object-contain hover:opacity-[0.7] cursor-pointer" width="200" height="80" />
+          <div class="relative">
+            <NuxtImg :src="trip.acf.explore_trip_mobile" alt="Druk Path Logo"
+              class="w-full object-contain hover:opacity-[0.7] cursor-pointer" width="200" height="80" />
+            <!-- The text will appear at the bottom of the image on mobile -->
+            <div class="absolute bottom-0 left-0 w-full p-2 py-6">
+              <p class="text-white uppercase text-lg font-bold tracking-wider" style="line-height: 20px;">
+                {{ trip.acf.trip_title }}
+              </p>
+            </div>
+          </div>
         </NuxtLink>
 
       </div>
