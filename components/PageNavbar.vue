8aa97c5359ad9033c2db84550cdc71ecba80a7c2
ameerhamza6273 <133323311+ameerhamza6273@users.noreply.github.com>
2025-04-12 17:51:04 +0500

    change

--- components/PageNavbar.vue
+++ components/PageNavbar.vue
@@ -1,111 +1,79 @@
 <template>
   <div v-if="headerData">
     <!-- Desktop & Tablet Navbar -->
-    <nav
-      :class="[
-        'fixed w-full z-50 top-0 left-0 text-white transition-all duration-300',
-        isScrolled ? 'bg-[#161616] shadow-[0_4px_10px_rgba(255,255,255,0.2)]' : 'bg-transparent'
-      ]"
-    >
+    <nav :class="[
+      'fixed w-full z-50 top-0 left-0 text-white transition-all duration-300',
+      isScrolled ? 'bg-[#161616] shadow-[0_4px_10px_rgba(255,255,255,0.2)]' : 'bg-transparent'
+    ]">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-5 px-5 sm:px-8">
         <!-- Logo -->
         <NuxtLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <NuxtImg :src="headerData.acf.logo" class="w-16 md:w-24" alt="Logo" />
         </NuxtLink>
+        
 
         <!-- Mobile Menu Toggle -->
-        <button
-          class="inline-flex items-center p-2 w-10 h-10 rounded-lg focus:outline-none md:hidden"
-          @click="toggleMenu"
-        >
+        <button class="inline-flex items-center p-2 w-10 h-10 rounded-lg focus:outline-none md:hidden"
+          @click="toggleMenu">
           <span class="sr-only">Open main menu</span>
-          <svg
-            v-if="!isToggled"
-            class="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
+          <svg v-if="!isToggled" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
-          <span v-else class="text-3xl font-semibold">×</span>
+          <span v-else class="text-4xl font-medium">×</span>
         </button>
 
         <!-- Desktop Menu -->
         <div class="hidden md:flex items-center space-x-6">
           <ul class="flex items-center space-x-5">
             <li>
-              <NuxtLink
-                :to="headerData.acf.link_1"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_1
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_1)" 
-              >
-              home 
+              <NuxtLink :to="headerData.acf.link_1" :class="[
+                'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
+                activeLink === headerData.acf.link_1
+                  ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
+                  : 'text-white'
+              ]" @click="getActiveLink(headerData.acf.link_1)">
+                home
               </NuxtLink>
             </li>
             <li>
-              <NuxtLink
-                :to="headerData.acf.link_2"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_2
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_2)" 
-              >
-              About Us
+              <NuxtLink :to="headerData.acf.link_2" :class="[
+                'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
+                activeLink === headerData.acf.link_2
+                  ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
+                  : 'text-white'
+              ]" @click="getActiveLink(headerData.acf.link_2)">
+                About Us
               </NuxtLink>
             </li>
             <li>
-              <NuxtLink
-                :to="headerData.acf.link_3"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_3
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_3)" 
-              >
-              Reviews
+              <NuxtLink :to="headerData.acf.link_3" :class="[
+                'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
+                activeLink === headerData.acf.link_3
+                  ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
+                  : 'text-white'
+              ]" @click="getActiveLink(headerData.acf.link_3)">
+                Reviews
               </NuxtLink>
             </li>
             <li>
-              <NuxtLink
-                :to="headerData.acf.link_4"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_4
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_4)" 
-              >
-              Contact Us
+              <NuxtLink :to="headerData.acf.link_4" :class="[
+                'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
+                activeLink === headerData.acf.link_4
+                  ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
+                  : 'text-white'
+              ]" @click="getActiveLink(headerData.acf.link_4)">
+                Contact Us
               </NuxtLink>
             </li>
-            <li >
-              <NuxtLink
-                :to="headerData.acf.link_5"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_5
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_5)"
-              >
-                  <NuxtImg :src="headerData.acf.icon" alt="icon" 
-                  class="inline w-7 mr-2"
-                  :class="activeLink === headerData.acf.link_5 ? ' filter brightness-50' : ''"
-                   />
+            <li>
+              <NuxtLink :to="headerData.acf.link_5" :class="[
+                'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
+                activeLink === headerData.acf.link_5
+                  ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
+                  : 'text-white'
+              ]" @click="getActiveLink(headerData.acf.link_5)">
+                <NuxtImg :src="headerData.acf.icon" alt="icon" class="inline w-7 mr-2"
+                  :class="activeLink === headerData.acf.link_5 ? ' filter brightness-50' : ''" />
                 Find A Trip
               </NuxtLink>
             </li>
@@ -115,94 +83,74 @@
     </nav>
 
     <!-- Mobile Navbar -->
-    <div
-      v-if="isToggled"
-      class="fixed inset-0 z-40 bg-[#000] flex items-center justify-center md:hidden"
-      
-    >
-      <ul class="space-y-4 text-center text-white">
+    <div v-if="isToggled" class="fixed inset-0 z-50 bg-[#000] flex flex-col justify-between pb-5 md:hidden">
+      <!-- Close button -->
+      <div class="text-white flex items-center justify-between p-4">
+        <div class=" md:hidden ">MENU</div>
+        <button @click="closeMenu" class="text-4xl">&times;</button>
+      </div>
+
+      <!-- Navigation links -->
+      <ul class="space-y-4 text-center text-white px-6">
         <li>
-              <NuxtLink
-                :to="headerData.acf.link_1"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_1
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_1); closeMenu()" 
-              >
-              home 
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink
-                :to="headerData.acf.link_2"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_2
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_2); closeMenu()" 
-              >
-              About Us
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink
-                :to="headerData.acf.link_3"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_3
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_3); closeMenu()" 
-              >
-              Reviews
-              </NuxtLink>
-            </li>
-            <li>
-              <NuxtLink
-                :to="headerData.acf.link_4"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_4
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_4); closeMenu()" 
-              >
-              Contact Us
-              </NuxtLink>
-            </li>
-            <li >
-              <NuxtLink
-                :to="headerData.acf.link_5"
-                :class="[
-                  'py-2 px-3 rounded-full font-medium uppercase transition-colors text-sm duration-300',
-                  activeLink === headerData.acf.link_5
-                    ? isScrolled ? 'text-[#dddddd8a] font-bold' : 'text-black font-bold'
-                    : 'text-white'
-                ]"
-                @click="getActiveLink(headerData.acf.link_5); closeMenu()"
-              >
-                  <NuxtImg :src="headerData.acf.icon" alt="icon" 
-                  class="inline w-7 mr-2"
-                  :class="activeLink === headerData.acf.link_5 ? ' filter brightness-50' : ''"
-                   />
-                Find A Trip
-              </NuxtLink>
-            </li>
+          <NuxtLink :to="headerData.acf.link_1" class="py-2 my-2 block"
+            :class="activeLink === headerData.acf.link_1 ? 'text-[#000] font-bold bg-[#D9D9D9]' : 'text-[#fff] font-bold bg-transparent'"
+            @click="getActiveLink(headerData.acf.link_1); closeMenu()">
+            <NuxtImg :src="headerData.acf.home_icon" alt="icon" class="inline h-4 mr-2"
+                  :class="activeLink === headerData.acf.link_1 ? ' filter brightness-0' : ''" />
+            
+            HOME
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="headerData.acf.link_2" class="py-2 my-2 block"
+            :class="activeLink === headerData.acf.link_2 ? 'text-[#000] font-bold bg-[#D9D9D9]' : 'text-[#fff] font-bold bg-transparent'"
+            @click="getActiveLink(headerData.acf.link_2); closeMenu()">
+            <NuxtImg :src="headerData.acf.about_icon" alt="icon" class="inline h-4 mr-2"
+                  :class="activeLink === headerData.acf.link_2 ? ' filter brightness-0' : ''" />
+            ABOUT US
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="headerData.acf.link_3" class="py-2 my-2 block"
+            :class="activeLink === headerData.acf.link_3 ? 'text-[#000] font-bold bg-[#D9D9D9]' : 'text-[#fff] font-bold bg-transparent'"
+            @click="getActiveLink(headerData.acf.link_3); closeMenu()">
+            <NuxtImg :src="headerData.acf.review_icon" alt="icon" class="inline h-4 mr-2"
+                  :class="activeLink === headerData.acf.link_3 ? ' filter brightness-0' : ''" />
+            REVIEWS
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="headerData.acf.link_5" class="py-2 my-2 block"
+            :class="activeLink === headerData.acf.link_5 ? 'text-[#000] font-bold bg-[#D9D9D9]' : 'text-[#fff] font-bold bg-transparent'"
+            @click="getActiveLink(headerData.acf.link_5); closeMenu()">
+            <NuxtImg :src="headerData.acf.icon" alt="icon" class="inline h-4 mr-2"
+              :class="activeLink === headerData.acf.link_5 ? ' filter brightness-0' : ''" />
+            FIND A TRIP
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink :to="headerData.acf.link_4" class="py-2 my-2 block"
+            :class="activeLink === headerData.acf.link_4 ? 'text-[#000] font-bold bg-[#D9D9D9]' : 'text-[#fff] font-bold bg-transparent'"
+            @click="getActiveLink(headerData.acf.link_4); closeMenu()">
+            <NuxtImg :src="headerData.acf.contact_icon" alt="icon" class="inline h-4 mr-2"
+                  :class="activeLink === headerData.acf.link_4 ? ' filter brightness-0' : ''" />
+            CONTACT US
+          </NuxtLink>
+
+        </li>
+
       </ul>
+      <NuxtLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse mx-auto">
+        <NuxtImg :src="headerData.acf.logo" class="w-16 md:w-24" alt="Logo" />
+      </NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup>
 import { computed } from "vue";
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
 
 const isToggled = ref(false);
 const isScrolled = ref(false);
@@ -242,6 +190,13 @@ const props = defineProps({
   }
 });
 const headerData = computed(() => props.headerData?.find(member => member.id === 27325) || null);
+watch(isToggled, (newVal) => {
+  if (newVal) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
+});
 
 </script>
 
